d619d200cd2de3402c4c482dcbfaf059ba0ae77f
棚田梨公 <>
2021-03-29 16:28:41 +0900

    Likeモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -32,7 +33,6 @@ class User < ApplicationRecord
   end
 
   def display_name
-    #ぼっち演算子
     profile&.nickname || self.email.split('@').first
   end
 
